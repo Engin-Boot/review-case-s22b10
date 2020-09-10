16bfe18470ff35bf12d912c6a29a47ed9d519c40
GaganPunjabi1 <gagan.punjabi@philips.com>
2020-09-10 17:41:09 +0530

    sender.cs

--- sender/Sender.cs
+++ sender/Sender.cs
@@ -2,16 +2,4 @@
 
 public class Class1
 {
-	void sender()
-    {
-        var x = 10;
-        var y = 20;
-        Console.WriteLine(x + y);
-    }
-    void dup_sender()
-    {
-        var x = 10;
-        var y = 20;
-        Console.WriteLine(x + y);
-    }
 }
