5d7332166cc9efaf418317bfdc2022ce2fc1e9f0
GaganPunjabi1 <gagan.punjabi@philips.com>
2020-09-10 16:49:17 +0530

    dup_check

--- sender/Sender.cs
+++ sender/Sender.cs
@@ -2,7 +2,16 @@
 
 public class Class1
 {
-	public Class1()
-	{
-	}
+	void sender()
+    {
+        var x = 10;
+        var y = 20;
+        Console.WriteLine(x + y);
+    }
+    void dup_sender()
+    {
+        var x = 10;
+        var y = 20;
+        Console.WriteLine(x + y);
+    }
 }
