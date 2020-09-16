7780fd39065ed9293dadd4bae0ed2369159bea07
GaganPunjabi1 <gagan.punjabi@philips.com>
2020-09-16 16:43:32 +0530

    sender

---
+++ Receiver.Test/TestCsvDataProcessor.cs
@@ -0,0 +1,18 @@
+﻿using ConsoleApp1;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Receiver.Test
+{
+    public class TestCsvDataProcessor
+    {
+        [Fact]
+        public void TestAppendDateInListIfNotInList()
+        {
+            CSVDataManipulator.AppendDateInListIfNotInList("04-02-2017");
+
+        }
+    }
+}
