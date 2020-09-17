2b86ba0bd5cfa2fe8882a31342ec2fb8df524740
kavyashukla18 <kavya.shukla@philips.com>
2020-09-17 09:50:16 +0530

    test

---
+++ Receiver.Test/TestHandleError.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using static Receiver.Test.ReceiverTestUtility;
+namespace Receiver.Test
+{
+    public class TestHandleError
+    {
+        [Fact]
+        public void ConsolerReaderForTestWithErrorCode()
+        {
+            var output = ConsolerReaderForTest();
+            HandleError.IfErrorLogInConsole("2(0xA)");
+            Assert.Equal("Error: Invalid column argument\r\n", output.ToString());
+            output.Close();
+        }
+        [Fact]
+        public void TestErrorLogInConsoleWithNoErrorCode()
+        {
+            var output = ConsolerReaderForTest();
+            HandleError.IfErrorLogInConsole("");
+            Assert.Equal("", output.ToString());
+            output.Close();
+        }
+    }
+}
