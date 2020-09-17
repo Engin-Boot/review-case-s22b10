b81780886cdaea61ac1b32df6e433e79ddc7ee09
kavyashukla18 <69622475+kavyashukla18@users.noreply.github.com>
2020-09-17 11:30:17 +0530

    Create TestHandleError.cs

---
+++ Receiver.Test/TestHandleError.cs
@@ -0,0 +1,25 @@
+using Xunit;
+using  static Receiver.Test.ReceiverTestUtility;
+
+namespace Receiver.Test
+{
+    public class TestHandleError
+    {
+        [Fact]
+        public void ConsolerReaderToTestReciverWithErrorCode()
+        {
+            var output = ConsolerReaderToTestReciver();
+            HandleError.IfErrorLogInConsole("2(0xA)");
+            Assert.Equal("Error: Invalid column argument\r\n", output.ToString());
+            output.Close();
+        }
+        [Fact]
+        public void TestErrorLogInConsoleWithNoErrorCode()
+        {
+            var output = ConsolerReaderToTestReciver();
+            HandleError.IfErrorLogInConsole("");
+            Assert.Equal("", output.ToString());
+            output.Close();
+        }
+    }
+}
