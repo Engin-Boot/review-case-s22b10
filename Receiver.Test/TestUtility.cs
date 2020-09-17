cd80631edc335c5e0835cddd83ba28888ded3319
kavyashukla18 <kavya.shukla@philips.com>
2020-09-17 15:01:17 +0530

    unit test

---
+++ Receiver.Test/TestUtility.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Xunit;
+using static Receiver.Test.ReceiverTestUtility;
+
+namespace Receiver.Test
+{
+    public class TestUtility
+    {
+        [Fact]
+        public void TestCreateFile()
+        {
+            string fileName = "TestCreateFile.csv";
+            bool isFileCreated = Utility.CreateFile(fileName);
+            string currDir = Directory.GetCurrentDirectory();
+            string filePath = Path.Combine(currDir, fileName);
+            if(File.Exists(filePath))
+                Assert.True(isFileCreated);
+            else
+                Assert.True(isFileCreated);
+        }
+
+        [Fact]
+        public void TestReadFromFile()
+        {
+            string fileName = "TestReadFromFile.csv";
+            StreamReader sr = new StreamReader(fileName);
+            Dictionary<string, CsvDataStructure> fileContent = new Dictionary<string, CsvDataStructure>();
+            Dictionary<string, CsvDataStructure> output = Utility.ReadFromFile(sr, fileContent);
+            Assert.NotEmpty(output);
+        }
+    }
+}
