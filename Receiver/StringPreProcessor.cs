d8a857de09c0b9eaa71e776428b4da3af0a19774
kavyashukla18 <kavya.shukla@philips.com>
2020-09-17 16:25:42 +0530

    changes

--- Receiver/StringPreProcessor.cs
+++ Receiver/StringPreProcessor.cs
@@ -96,7 +96,7 @@ namespace Receiver
         private static bool RemoveStopWord(string word)
         {
             StreamReadWrite srw = new StreamReadWrite();
-            string filePath = "C:\\Users\\320089145\\trainging\\ConsoleApp1\\ConsoleApp1\\bin\\Debug\\stopwords.csv";
+            string filePath = "C:\\Users\\320089145\\trainging\\review-case-s22b10\\Receiver\\Properties\\stopwords.csv";
             bool isFilePathExist = File.Exists(filePath);
             if (isFilePathExist)
             {
